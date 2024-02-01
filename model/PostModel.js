8c6f82414c5bc058b2dacd5649295cbe8fc0f3e2
mukeshkumargiri523 <mukeshkumargiri523@gmail.com>
2024-02-01 20:07:35 +0530

    first commit

---
+++ model/PostModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const PostSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+
+      ref: "Users",
+    },
+    desc: String,
+    likes: [],
+    image: String,
+  },
+  { timestamps: true }
+);
+
+const PostModel = mongoose.model("Posts", PostSchema);
+export default PostModel;
